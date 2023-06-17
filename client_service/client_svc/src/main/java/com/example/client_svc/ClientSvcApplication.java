b109c871d28259a30c86bcd5c59e71505f91302e
jenniferoentoro <jenniferoentoro@gmail.com>
2023-06-18 00:42:45 +0700

    updet

--- client_service/client_svc/src/main/java/com/example/client_svc/ClientSvcApplication.java
+++ client_service/client_svc/src/main/java/com/example/client_svc/ClientSvcApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClientSvcApplication {
 
 	static final String topicExchangeName = "eo-exchange";
-
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(ClientSvcApplication.class, args);
 	}
