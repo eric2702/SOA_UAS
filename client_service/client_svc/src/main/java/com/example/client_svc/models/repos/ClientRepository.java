d833884e0016e40e926ac8bdbbba312309da9933
Eric <3ric8t@gmail.com>
2023-06-17 17:33:57 +0700

    fixeddd!!!

--- client_service/client_svc/src/main/java/com/example/client_svc/models/repos/ClientRepository.java
+++ client_service/client_svc/src/main/java/com/example/client_svc/models/repos/ClientRepository.java
@@ -1,10 +1,8 @@
 package com.example.client_svc.models.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.client_svc.models.entities.Client;
 
-@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
