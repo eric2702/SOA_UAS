173a9a7dfe6aa0d269a545966bbe6a8506d710d4
jenniferoentoro <jenniferoentoro@gmail.com>
2023-06-19 01:53:28 +0700

    nginx-setup

--- run-containers.sh
+++ run-containers.sh
@@ -7,3 +7,4 @@ docker-compose -f staff_service/docker-compose.yml up -d --build
 docker-compose -f login_service/docker-compose.yml up -d --build
 docker-compose -f order_service/docker-compose.yml up -d --build
 docker-compose -f event_service/docker-compose.yml up -d --build
+docker-compose -f web_ui_service/docker-compose.yml up -d --build
